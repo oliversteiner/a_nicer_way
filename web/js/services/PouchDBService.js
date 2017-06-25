2b8594bcb9a7ee8dbacd60a46395be02f5182012
Oliver Steiner <oliver@mollo.ch>
2017-06-25 18:04:34 +0200

    web

---
+++ web/js/services/PouchDBService.js
@@ -0,0 +1,249 @@
+/**
+ * Class DbController
+ *  - Sammelt alle Funktionen für die Speicherung der Daten
+ *
+ *
+ */
+// TODO ist ein Service, nicht Controller
+// Global
+var _jsonFile;
+var PouchDBService = (function () {
+    // Wird aufgerufen beim erstellen der Klasse (new pouchDBService)
+    function PouchDBService() {
+        $.getJSON("data/defaultData.json", function (json) {
+            _jsonFile = json;
+        });
+    }
+    /**
+     * addWayPoint
+     * - Einen TimeWayPoint in die Datenbank schreiben
+     *
+     * @param data
+     */
+    PouchDBService.addWayPoint = function (data) {
+        // Rückgabewert ist als Falsch voreingestellt
+        var status = false;
+        var db = new PouchDB('anicerway');
+        var timeWayPoint;
+        // Datensatz zusammenstellen
+        timeWayPoint = {
+            _id: 'TimeWayPoint-' + new Date().toISOString(),
+            active: true,
+        };
+        // alle Werte von "data" in  "doc" fügen.
+        for (var key_1 in data) {
+            if (data.hasOwnProperty(key_1)) {
+                var value = data[key_1];
+                if (key_1 != "_id") {
+                    timeWayPoint[key_1] = value;
+                }
+            }
+        }
+        // Datensatz in die DB speichern
+        db.put(timeWayPoint).then(function (response) {
+            // handle response
+            setTimeout(function () {
+                pouchDBService.sync();
+            }, 1000);
+        }).catch(function (err) {
+            console.error(err);
+        });
+        return status;
+    };
+    /**
+     *
+     * updateWayPoint
+     *  - ändert einen Eintrag in der DB
+     *
+     * @param data
+     *
+     */
+    PouchDBService.updateWayPoint = function (data) {
+        var status = false;
+        var db = new PouchDB('anicerway');
+        // Dokument laden, dann updaten
+        db.get(data._id).catch(function (err) {
+            if (err.name === 'not_found') {
+                console.log('- not found');
+                return {};
+            }
+            else {
+                throw err;
+            }
+        }).then(function (doc) {
+            for (var key_2 in data) {
+                if (data.hasOwnProperty(key_2)) {
+                    var value = data[key_2];
+                    if (key_2 != "_id") {
+                        doc[key_2] = value;
+                    }
+                }
+            }
+            pouchDBService.sync();
+            return db.put(doc);
+        }).catch(function (err) {
+            // handle any errors
+            console.error(err);
+        });
+        return status;
+    };
+    /**
+     * loadAllWayPoints
+     *  - holt alle Waypoints aus der Datenbank
+     *
+     *
+     */
+    PouchDBService.loadAllWayPoints = function () {
+        var db = new PouchDB('anicerway');
+        var docs = db.allDocs({
+            include_docs: true,
+            startkey: 'TimeWayPoint',
+            endkey: 'TimeWayPoint\uffff'
+        }).then(function (result) {
+            // sort
+            return result;
+            // handle result
+        }).catch(function (err) {
+            console.log(err);
+        });
+        // Auf die Zeilen kann zugegriffen werden über:
+        //    .then( function(doc){ doc.rows })
+        return docs;
+    };
+    /**
+     * loadWayPoint
+     *  - lädt nur den TimeWayPoint mit der gewählten ID
+     *
+     * @param id
+     */
+    PouchDBService.loadWayPoint = function (id) {
+        var db = new PouchDB('anicerway');
+        var doc = db.get(id).catch(function (err) {
+            if (err.name === 'not_found') {
+                console.log('- not found');
+                return {};
+            }
+            else {
+                throw err;
+            }
+        }).then(function (doc) {
+            return doc;
+        }).catch(function (err) {
+            // handle any errors
+            console.error(err);
+        });
+        // Auf das Dokument kann zugegriffen werden über:
+        //    .then( function(doc){ doc })
+        return doc;
+    };
+    /**
+     * deleteWayPoint
+     * - Löscht einen TimeWayPoint aus der DB
+     *
+     * @param id
+     *
+     */
+    PouchDBService.deleteWayPoint = function (id) {
+        var status = true;
+        var db = new PouchDB('anicerway');
+        // Dokument laden, dann den Löschbefehl schicken
+        db.get(id).catch(function (err) {
+            if (err.name === 'not_found') {
+                console.log('- not found');
+                return {};
+            }
+            else {
+                throw err;
+            }
+        }).then(function (doc) {
+            doc._deleted = true;
+            pouchDBService.sync();
+            return db.put(doc);
+        }).catch(function (err) {
+            // handle any errors
+            console.error(err);
+        });
+        return status;
+    };
+    /**
+     * sync
+     *  - synchronisiert die lokale DB im Browser mit der BS-datenbank (couchDB)
+     */
+    PouchDBService.sync = function () {
+        var remote = 'http://localhost:5984/anicerway';
+        var sync_options = {
+            live: true,
+            retry: true,
+            continuonus: true
+        };
+        var db = new PouchDB('anicerway');
+        db.sync(remote, sync_options);
+    };
+    /**
+     *
+     *
+     */
+    PouchDBService.eraseDB = function () {
+        var db = new PouchDB('anicerway');
+        db.destroy().then(function () {
+            // database destroyed
+            console.log('database destroyed');
+        }).catch(function (error) {
+            // error occurred
+        });
+    };
+    PouchDBService.loadDefault = function () {
+        console.log('neue DB Einträge');
+        // Musterdaten:
+        var db = new PouchDB('anicerway');
+        // Aktuelle Zeit
+        var timestamp = Date.now();
+        // Vorgaben für die Daten
+        db.bulkDocs(_jsonFile.data).then(function (result) {
+            // handle result
+            console.log("mustereintraege");
+            console.log(result);
+        }).catch(function (error) {
+            console.log(error);
+            console.log("defaultData exists already");
+        });
+        // define _design Configuration
+        var designDocConfiguration = {
+            _id: '_design/Configuration',
+            views: {
+                "Configuration": {
+                    "map": "function (doc, meta) {  if (doc.type == 'Configuration') {   emit(doc.createdOn,doc);  }}"
+                }
+            }
+        };
+        // define _design TimeWayPoint
+        var designDocTimeWayPoint = {
+            _id: '_design/TimeWayPoint',
+            views: {
+                "TimeWayPoint": {
+                    "map": "function (doc, meta) {  if (doc.type == 'TimeWayPoint') {   emit(doc.createdOn,doc);  }}"
+                }
+            }
+        };
+        // put _designs
+        // kunde
+        db.put(designDocConfiguration).then(function (info) {
+            console.log("Design Doc 'Configuration' created");
+            // design doc created
+        }).catch(function (error) {
+            console.log("design doc 'Configuration' already exists");
+            // if err.name === 'conflict', then
+            // design doc already exists
+        });
+        // TimeWayPoint
+        db.put(designDocTimeWayPoint).then(function (info) {
+            console.log("Design Doc 'TimeWayPoint' created");
+            // design doc created
+        }).catch(function (error) {
+            console.log("design doc 'TimeWayPoint' already exists");
+            // if err.name === 'conflict', then
+            // design doc already exists
+        });
+    };
+    return PouchDBService;
+}());
---
+++ web/js/services/SocketIOService.js
@@ -0,0 +1,139 @@
+var SocketIOService = (function () {
+    function SocketIOService() {
+        if (aNicerWay.socket_io) {
+            this.socket = io();
+            this.receiveCommand();
+            this.receivePing();
+            this.receiveMessage();
+            this.receiveTimepointId();
+        }
+        else {
+            console.warn('kein Socket IO');
+            var text = '<span class="small">Eingeschränkte DEMO:<br> ' + 'Die App läuft nicht auf einem <i>Socket IO</i> fähigen Server.</span>';
+            $('#remote-list-header').html(text);
+        }
+    }
+    // SEND
+    SocketIOService.prototype.send = function (type, msg) {
+        console.log('aNicerWay.socket_io: ' + aNicerWay.socket_io);
+        if (aNicerWay.socket_io === false) {
+            // Nachricht schicken
+            socketSimulatorService.receive(type, msg);
+        }
+        else {
+            this.socket.emit(type, msg);
+        }
+    };
+    SocketIOService.prototype.directSend = function (type, msg) {
+        // Nachricht schicken
+        console.log(type, msg);
+    };
+    SocketIOService.prototype.sendPing = function () {
+        var png = 'PING';
+        // Nachricht schicken
+        this.send('ping_2', png);
+    };
+    SocketIOService.prototype.sendList = function () {
+        var list = aNicerWay.getTimeWayPointList();
+        // liste schicken
+        this.send('timepoint list', list);
+    };
+    SocketIOService.prototype.sendTimePointNr = function (nr) {
+        this.send('timepoint nr', nr);
+    };
+    // RECEIVE
+    SocketIOService.prototype.receiveCommand = function () {
+        /* - socket-cmd ----------------
+         previous
+         next
+
+         navigation-display-toggle
+         data-display-toggle
+         smartphone-sim-toggle
+         console-toggle
+         remote-open
+         help-toggle
+         ------------------------------
+         */
+        this.socket.on('command', function (cmd) {
+            console.log(cmd);
+            var found_action = cmd.search('action-');
+            var found_character = cmd.search('character-');
+            // Nach "action-" suchen
+            if (found_action === 0) {
+                var action = cmd.replace('action-', '');
+                aNicerWay.characterController.action(action);
+            }
+            else if (found_character === 0) {
+                var character_id = cmd.replace('character-', '');
+                aNicerWay.characterController.changeCharacter(character_id);
+            }
+            else {
+                switch (cmd) {
+                    case 'previous':
+                        aNicerWay.goToPrevious();
+                        break;
+                    case 'next':
+                        aNicerWay.goToNext();
+                        break;
+                    case 'first':
+                        aNicerWay.goToFirst();
+                        break;
+                    case 'last':
+                        aNicerWay.goToLast();
+                        break;
+                    // Toolbar
+                    case 'navigation-display-toggle':
+                        aNicerWay.displayController.toggle('navigation');
+                        break;
+                    case 'data-display-toggle':
+                        aNicerWay.displayController.toggle('data');
+                        break;
+                    case 'smartphone-sim-toggle':
+                        SmartphoneSimController.toggle();
+                        break;
+                    case 'console-toggle':
+                        ConsoleController.modalToggle();
+                        break;
+                    case 'remote-toggle':
+                        aNicerWay.displayController.toggle('remote');
+                        break;
+                    case 'help-toggle':
+                        HelpController.modalToggle();
+                        break;
+                    case 'get-list':
+                        socketIOService.sendList();
+                        break;
+                    case 'dont-talk':
+                        aNicerWay.characterController.dontTalk();
+                        break;
+                    default:
+                        SmartphoneSimController.error('Remotebefehl nicht verstanden');
+                        SmartphoneSimController.error('> ' + cmd, 1);
+                }
+            }
+        });
+    };
+    SocketIOService.prototype.receivePing = function () {
+        // ping_2 empfangen
+        this.socket.on('ping_2', function () {
+            console.log('PING');
+            SmartphoneSimController.ping();
+        });
+    };
+    SocketIOService.prototype.receiveMessage = function () {
+        // Nachricht empfangen
+        this.socket.on('chat message', function (msg) {
+            console.log(msg);
+            SmartphoneSimController.message(msg);
+            aNicerWay.characterController.talk(msg);
+        });
+    };
+    SocketIOService.prototype.receiveTimepointId = function () {
+        // Nachricht empfangen
+        this.socket.on('timepoint id', function (id) {
+            aNicerWay.goTo(id);
+        });
+    };
+    return SocketIOService;
+}());

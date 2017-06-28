3a4690f95d657f91739e4b53831e64119d1c41ef
Oliver Steiner <oliver@mollo.ch>
2017-06-28 15:30:51 +0200

    bilder

---
+++ web/js/editor/controllers/EditorToolbarSideController.js
@@ -0,0 +1,8 @@
+var EditorToolbarSideController = (function () {
+    function EditorToolbarSideController() {
+        console.log('EditorToolbarSideController');
+    }
+    EditorToolbarSideController.prototype.get = function () {
+    };
+    return EditorToolbarSideController;
+}());
